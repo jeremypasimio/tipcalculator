5e0f28508f00e9a5b1e01645e865e1efba0cf119
jeremypasimio <Jeremypasimio@gmail.com>
2020-06-09 12:22:39 -0700

    Update Partner List

--- DisbursementPageCode.js
+++ DisbursementPageCode.js
@@ -1,8 +1,8 @@
 "use strict"
 
 const partners = ['Bethany', 'Joey', 'Cheryl', 'Diego', 'Julia', 'Tabby',
-    'Elliot', 'Valerie', 'Delaney', 'Avi', 'Jeremy', 'Mekhi',
-    'Codi', 'Reese', 'Megan','Bella', 'Camden', 'Grace'];
+    'Elliot', 'Rachel','Valerie', 'Delaney', 'Avi', 'Jeremy', 'Mekhi',
+    'Codi', 'Reese', 'Stacie','Megan','Samantha','Bella', 'Camden', 'Grace'];
 const tips = JSON.parse(localStorage.tipTotal);
 const tipTotal = tips.total;
 var payoutList = [];
